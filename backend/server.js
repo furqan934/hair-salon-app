7333a8f5248518cc4b4bdd61ba7f90e5c60ceaac
furqan934 <furqanmf234@gmail.com>
2026-01-14 23:20:07 +0500

    little changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const app = express();
 const connectDB = require('./src/config/db');
 
 // Load env
@@ -9,15 +10,17 @@ dotenv.config();
 // Connect MongoDB
 connectDB();
 
-const app = express();
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+
 // Routes
 app.use('/api/auth', require('./src/routes/auth'));
 app.use('/api/appointments', require('./src/routes/appointments'));
+
 
 // Global error handler
 app.use((err, req, res, next) => {
